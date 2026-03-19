2d9f0c3961d1c3b38227c1522e3036839c2aa2d2
Yaroslav Veretelnyk <s33008@pjwstk.edu.pl>
2026-03-19 22:42:40 +0100

    Change msg to deez on feat conf

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proj");
+        System.out.println("Deez");
 
         // definitely added console input reading
 
