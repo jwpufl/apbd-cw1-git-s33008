7cd6936570e940a54dcaaea4192de7e7a28a88bb
Yaroslav Veretelnyk <s33008@pjwstk.edu.pl>
2026-03-19 22:43:09 +0100

    Change output message on main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proj");
+        System.out.println("DEezGi");
 
         // definitely added console input reading
 
